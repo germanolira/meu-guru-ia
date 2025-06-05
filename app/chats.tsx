81a75cf4dc32f671b79b321c6a2713ddde8ddcaa
Germano Lira <dolan.3@hotmail.com>
2025-06-05 04:14:09 -0300

    refactor: chat

---
+++ app/chats.tsx
@@ -0,0 +1,42 @@
+import { useRouter } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { ChatList } from '../components/chat/ChatList';
+import { useChatStorage } from '../hooks/useChatStorage';
+
+export default function ChatsScreen() {
+  const router = useRouter();
+  const {
+    chats,
+    currentChatId,
+    isLoading,
+    createNewChat,
+    switchToChat,
+    removeChatById,
+  } = useChatStorage();
+
+  const handleChatSelect = (chatId: string) => {
+    switchToChat(chatId);
+    router.push('/');
+  };
+
+  const handleNewChat = () => {
+    createNewChat();
+    router.push('/');
+  };
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <StatusBar style="auto" />
+      <ChatList
+        chats={chats}
+        currentChatId={currentChatId}
+        isLoading={isLoading}
+        onChatSelect={handleChatSelect}
+        onNewChat={handleNewChat}
+        onDeleteChat={removeChatById}
+      />
+    </SafeAreaView>
+  );
+} 
