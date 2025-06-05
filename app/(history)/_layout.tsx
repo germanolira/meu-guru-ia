b404b344d7013b310e916fe22dfe24a470e0d02e
Germano Lira <dolan.3@hotmail.com>
2025-06-05 18:08:46 -0300

    feat: create app chat history screen

---
+++ app/(history)/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function HistoryLayout() {
+  return <Stack screenOptions={{ headerShown: false, presentation: "modal" }} />;
+} 
