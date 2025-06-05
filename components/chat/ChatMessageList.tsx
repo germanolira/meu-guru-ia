d3081ccacdecf07a0830d780d81d7eb5bc9cf1ac
Germano Lira <dolan.3@hotmail.com>
2025-06-05 03:08:36 -0300

    feat: create chatmessagelist

--- components/chat/ChatMessageList.tsx
+++ components/chat/ChatMessageList.tsx
@@ -1,38 +1,51 @@
-import {
-  LegendList,
-  LegendListRef,
-  LegendListRenderItemProps,
-} from "@legendapp/list";
-import React from "react";
-import { View } from "react-native";
+import { LegendList, LegendListRef } from "@legendapp/list";
+import React, { forwardRef, useCallback } from "react";
+import { Keyboard, View } from "react-native";
+import { useAnimatedKeyboard, useAnimatedStyle } from "react-native-reanimated";
 import { Message } from "../../types/chat";
 import { ChatMessage } from "./ChatMessage";
 
 interface ChatMessageListProps {
   messages: Message[];
+  chatInputHeight?: number;
 }
 
-export const ChatMessageList = React.forwardRef(function ChatMessageList(
-  { messages }: ChatMessageListProps,
-  ref: React.Ref<LegendListRef>
-) {
-  const renderMessage = ({ item }: LegendListRenderItemProps<Message>) => {
-    return <ChatMessage message={item} />;
-  };
+export const ChatMessageList = forwardRef<LegendListRef, ChatMessageListProps>(
+  ({ messages, chatInputHeight = 70 }, ref) => {
+    const keyboard = useAnimatedKeyboard();
 
-  return (
-    <View className="flex-1 px-4">
-      <LegendList
-        data={messages}
-        renderItem={renderMessage}
-        keyExtractor={(item) => item.id}
-        recycleItems={true}
-        maintainVisibleContentPosition
-        ref={ref}
-        className="py-2"
-      />
-    </View>
-  );
-});
+    const animatedFlatListPadding = useAnimatedStyle(() => {
+      const padding = keyboard.height.value + chatInputHeight + 10;
+      return {
+        paddingBottom: padding,
+      };
+    }, [chatInputHeight]);
+
+    const renderItem = useCallback(
+      ({ item }: { item: Message }) => <ChatMessage message={item} />,
+      []
+    );
+
+    return (
+      <View className="flex-1">
+        <LegendList
+          ref={ref}
+          data={messages}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[
+            { paddingHorizontal: 16, paddingTop: 20, flexGrow: 1 },
+            animatedFlatListPadding,
+          ]}
+          keyboardDismissMode="interactive"
+          keyboardShouldPersistTaps="handled"
+          onScrollBeginDrag={Keyboard.dismiss}
+          invertStickyHeaders
+        />
+      </View>
+    );
+  }
+);
 
 ChatMessageList.displayName = "ChatMessageList";
